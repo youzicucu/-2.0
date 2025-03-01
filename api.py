3f6455fc19939d917bbc3c0a59fb929098159e37
youzicucu <gdworkzy@163.com>
2025-03-02 06:26:16 +0800

    Update api.py

--- api.py
+++ api.py
@@ -76,7 +76,7 @@ except Exception as e:
 # 加载中文别名
 def load_aliases():
     try:
-        df = pd.read_csv("data/team_aliases.csv", encoding=""latin1"")  # 如果文件包含 BOM 头
+        df = pd.read_csv("data/team_aliases.csv", encoding="latin1")  # 如果文件包含 BOM 头
         return {
             row['zh_name']: {
                 'id': row['id'],
